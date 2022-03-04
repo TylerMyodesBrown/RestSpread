bbaaf9910eb44a980ff491195d8f0dbe29bd86b8
Brown <Tyler.Brown02@dmschools.org>
2022-03-04 13:25:50 -0600

    forgot to save

--- RestSpread.js
+++ RestSpread.js
@@ -32,3 +32,44 @@ const doubleAndReturnArgs = (arr, ...args) =>{
     return [...arr, ...args.map(v => v *2)]
 }
 
+//removes random item then returns it.
+const removeRandom = items => {
+    let idx = Math.floor(Math.random() * items.length);
+    return [...items.slice(0, idx), ...items.slice(idx + 1)];
+  }
+
+//Return a new array with every item in array1 and array2.
+
+const extend = (array1, array2) => {
+    return [...array1, ...array2];
+  }
+  
+  //Return a new object with all the keys and values from obj and a new key/value pair
+  
+  const addKeyVal = (obj, key, val) => {
+    let newObj = { ...obj }
+    newObj[key] = val;
+    return newObj;
+  }
+  
+  //Return a new object with a key removed.
+  
+  const removeKey = (obj, key) => {
+    let newObj = {...obj}
+    delete newObj[key]
+    return newObj;
+  }
+  
+  //Combine two objects and return a new object.
+  
+  const combine = (obj1, obj2) => {
+    return {...obj1,...obj2};
+  }
+  
+//   Return a new object with a modified key and value. 
+  
+  const update = (obj, key, val) => {
+    let newObj = {...obj}
+    newObj[key] = val;
+    return newObj;
+  }
